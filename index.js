729465dc06a8fb3d4382b057bbf9e3b98919c7f1
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-09-07 21:28:25 +0600

    tird

--- index.js
+++ index.js
@@ -117,7 +117,73 @@ const displayYourCart = (name, taka) => {
 }
 
 //display details
+const displayDetails=async (id)=>{
+    await fetch(`https://openapi.programming-hero.com/api/plant/${id}`).then(res=>res.json()).then(json=>{
+        displayModal(json.plants)
+    })
+}
 
+//display the pop up modal
+const displayModal=(plants)=>{
+    // console.log(plants)
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML='';
+    const newDivContain = document.createElement('div');
+    newDivContain.innerHTML=`
+    <h1 class="text-xl font-bold">${plants.name}</h1>
+        <img class="h-[300px] w-full rounded-lg mt-3" src="${plants.image}" alt="">
+        <h2 class="mt-3"> <span class="font-semibold">Category:</span> ${plants.category}</h2>
+        <h3 class="mt-3"><span class="font-semibold">Price:</span> ৳${plants.price}</h3>
+        <h3 class="mt-3"><span class="font-semibold">Description:</span> ${plants.description}</h3>
+
+        <form method="dialog" class="modal-backdrop">
+        <button class="btn text-black mt-5 flex flex-row-reverse">close</button>
+
+    </form>
+    `
+    modalContainer.append(newDivContain)
+
+    document.getElementById('my_modal_2').showModal()
+}
+//display the cart section
+
+const displayTreeCart =(treeData)=>{
+
+    const allTrees = document.getElementById("all-tree-cart");
+    allTrees.innerHTML='';
+    
+    treeData.forEach(data => {
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML =`
+        <div class="bg-white p-5 space-y-10 flex flex-col h-full rounded-md ">
+            <img class="w-[311px] h-[186px] rounded-md" src="${data.image}" alt="">
+            <h1  onclick="displayDetails(${data.id})"  class="font-semibold">${data.name}</h1>
+            <p>${data.description}</p>
+            <div class="flex justify-between text-center items-center mt-auto ">
+                <button  class="btn btn-soft btn-success rounded-lg">${data.category}</button>
+                <p>৳${data.price}</p>
+
+            </div>
+            <button onclick="displayYourCart('${data.name}',${data.price})" class="btn btn-success rounded-lg w-full mt-auto ">Add to Cart</button>
+        </div>
+        `
+        allTrees.append(newDiv)
+    });
+    
+}
+
+// displaying category card 
+const displayCategory =(categoryData) =>{
+    const allCategorySection = document.getElementById("all-category-section");
+    allCategorySection.innerHTML='';
+    for(data of categoryData){
+        const addData = document.createElement('div')
+        addData.innerHTML=`
+        <button id="btn-category-click-${data.id}" onclick="categoryClicked(${data.id})" class="btn bg-transparent border-0 category-btn">${data.category_name}</button>
+        `
+        allCategorySection.append(addData)
+    }
+}
 
 loadCartSection()
 loadCategory()
